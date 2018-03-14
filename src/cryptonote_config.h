4b80afe091a2601f418ddd0fe73a659afd56565c
gazhayes <gareth.hayes@gmail.com>
2018-03-14 09:28:57 +0800

    play around with genesis message

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -158,17 +158,17 @@ namespace config
   uint64_t const BASE_REWARD_CLAMP_THRESHOLD = ((uint64_t)100000000); // pow(10, 8)
   std::string const P2P_REMOTE_DEBUG_TRUSTED_PUB_KEY = "0000000000000000000000000000000000000000000000000000000000000000";
 
-  uint64_t const CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX = 18;
-  uint64_t const CRYPTONOTE_PUBLIC_INTEGRATED_ADDRESS_BASE58_PREFIX = 19;
-  uint64_t const CRYPTONOTE_PUBLIC_SUBADDRESS_BASE58_PREFIX = 42;
-  uint16_t const P2P_DEFAULT_PORT = 19080;
-  uint16_t const RPC_DEFAULT_PORT = 18081;
-  uint16_t const ZMQ_RPC_DEFAULT_PORT = 18082;
+  uint64_t const CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX = 110;
+  uint64_t const CRYPTONOTE_PUBLIC_INTEGRATED_ADDRESS_BASE58_PREFIX = 111;
+  uint64_t const CRYPTONOTE_PUBLIC_SUBADDRESS_BASE58_PREFIX = 112;
+  uint16_t const P2P_DEFAULT_PORT = 12184;
+  uint16_t const RPC_DEFAULT_PORT = 12185;
+  uint16_t const ZMQ_RPC_DEFAULT_PORT = 12186;
   boost::uuids::uuid const NETWORK_ID = { {
-      0x11 ,0x30, 0xF1, 0x71 , 0x61, 0x04 , 0x41, 0x61, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x10
-    } }; // Bender's nightmare
-  std::string const GENESIS_TX = "023c01ff00010102008f283c6988c6ede1678065bd241fd9652517234858c0c66331f245b1c1873f21011d9b357ff5ca5ce06e670d6e4432907b94b9708b103f9e6c4c040cae2d02409700";
-  uint32_t const GENESIS_NONCE = 10000;
+      0x42, 0x4c, 0x4f, 0x43, 0x4b, 0x52, 0x41, 0x5a, 0x4f, 0x52
+    } };
+  std::string const GENESIS_TX = "023c01ff000101020d20f3f7233abb49c9f85c2223fd1dea8d603b0198e96261326031030154926421011dc8c7cae30981b4d72800b63b77dab91076a6157931e6392a69027afb2f52b000";
+  uint32_t const GENESIS_NONCE = 20161004;
 
   namespace testnet
   {
@@ -180,7 +180,7 @@ namespace config
     uint16_t const ZMQ_RPC_DEFAULT_PORT = 28082;
     boost::uuids::uuid const NETWORK_ID = { {
         0x12 ,0x30, 0xF1, 0x71 , 0x61, 0x04 , 0x41, 0x61, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x11
-      } }; // Bender's daydream
+      } };
     std::string const GENESIS_TX = "013c01ff0001ffffffffffff03029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121017767aafcde9be00dcfd098715ebcf7f410daebc582fda69d24a28e9d0bc890d1";
     uint32_t const GENESIS_NONCE = 10001;
   }
@@ -195,7 +195,7 @@ namespace config
     uint16_t const ZMQ_RPC_DEFAULT_PORT = 38082;
     boost::uuids::uuid const NETWORK_ID = { {
         0x12 ,0x30, 0xF1, 0x71 , 0x61, 0x04 , 0x41, 0x61, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x12
-      } }; // Bender's daydream
+      } };
     std::string const GENESIS_TX = "013c01ff0001ffffffffffff0302df5d56da0c7d643ddd1ce61901c7bdc5fb1738bfe39fbe69c28a3a7032729c0f2101168d0c4ca86fb55a4cf6a36d31431be1c53a3bd7411bb24e8832410289fa6f3b";
     uint32_t const GENESIS_NONCE = 10002;
   }
